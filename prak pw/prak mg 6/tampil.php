a48d4b7d5e7e2a90dab87d60f7e68c77c7327ab9
puay12 <puayyssii12@gmail.com>
2021-04-08 01:18:48 +0800

    modified:   prak pw/prak mg 6/form.css 	modified:   prak pw/prak mg 6/tampil.php

--- prak pw/prak mg 6/tampil.php
+++ prak pw/prak mg 6/tampil.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="form.css">
     <style>
         body{
-            background : rgba(0, 255, 255, 0.5);
+            background : rgba(0, 255, 255, 0.6);
         }
     </style>
 </head>
