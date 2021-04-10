96178759e1664c50ecdced0a09b09c7a2eca8a63
puay12 <puayyssii12@gmail.com>
2021-04-11 01:28:10 +0800

    modified:   prak pw/uts proyek/coba.php 	modified:   prak pw/uts proyek/tampil.php

--- prak pw/uts proyek/coba.php
+++ prak pw/uts proyek/coba.php
@@ -14,26 +14,11 @@
     <?php 
         include 'koneksi.php';
         $id = $_POST['id_mhs'];
-        $sql = "SELECT * FROM mahasiswa WHERE id_mhs=" . $id;
+        $sql = "SELECT * FROM mahasiswa WHERE id_mhs=$id";
         $result = mysqli_query($konek, $sql);
 
         if(mysqli_num_rows($result) > 0){
             while($kolom = mysqli_fetch_array($result)){
-                $nama = $kolom['nama'];
-                $tmptLahir = $kolom["tmptLAHIR"];
-                $tglLahir = $kolom["tglLAHIR"];
-                $gender = $kolom["gender"];
-                $alamat = $kolom["alamat"];
-                $jurusan = $kolom["jurusan"];
-                $lamaStudiThn = $kolom["lama-studi-thn"];
-                $lamaStudiBln = $kolom["lama-studi-bln"];
-                $ipk = $kolom["ipk"];
-                $deskripsi = $kolom["deskripsi"];
-            }
-        }
-        else{
-            echo "0 result";
-        }
     ?>
 	<!-- FORM -->
 	<div class="sec2 position-relative" id="sec2">
@@ -51,13 +36,13 @@
 								<div class="form-group">
 									<label for="nrp">NRP</label>
 									<input type="text" class="form-control" name="nrp" id="nrp" 
-                                    value="<?php echo $nrp;?>">
+                                    value="<?php $kolom['nrp']?>">
 								</div>
 								<!-- nama---------- -->
 								<div class="form-group">
 									<label for="nama">Nama Lengkap</label>
 									<input type="text" class="form-control" name="nama" id="nama" 
-                                    value="<?php echo $nama;?>">
+                                    value="<?php $kolom['nama']?>">
 								</div>
 								<!-- TTL----------- -->
 								<div class="form-group">
@@ -65,18 +50,18 @@
 									<div class="row">
 										<div class="col">
 											<input type="text" class="form-control" name="tmptLAHIR" id="ttl"
-												placeholder="Kota Kelahiran" value="<?php echo $tmptLahir;?>">
+												placeholder="Kota Kelahiran" value="<?php $kolom['tmpt_lahir']?>">
 										</div>
 										<div class="col">
 											<input type="date" class="form-control" name="tglLAHIR" id="ttl"
-												placeholder="Tanggal lahir" value="<?php echo $tglLahir;?>">
+												placeholder="Tanggal lahir" value="<?php $kolom['tgl_lahir']?>">
 										</div>
 									</div>
 								</div>
 								<!-- JENIS KELAMIN -->
 								<div class="form-group">
 									<label for="gender">Jenis Kelamin</label>
-									<select class="form-control" id="gender" name="gender" value="<?php echo $gender;?>">
+									<select class="form-control" id="gender" name="gender" value="<?php $kolom['gender']?>">
 										<option value="Laki-laki">Laki-laki</option>
 										<option value="Perempuan">Perempuan</option>
 									</select>
@@ -84,7 +69,7 @@
 								<!-- AGAMA---------- -->
 								<div class="form-group">
 									<label for="agama">Agama</label>
-									<select class="form-control" id="agama" name="agama" value="<?php echo $agama;?>">
+									<select class="form-control" id="agama" name="agama" value="<?php $kolom['agama']?>">
 										<option value="Islam">Islam</option>
 										<option value="Kristen Protestan">Kristen Protestan</option>
                                         <option value="Kristen Katolik">Kristen Katolik</option>
@@ -98,7 +83,7 @@
 								<div class="form-group">
 									<label for="alamat">Alamat Tempat Tinggal</label>
 									<input type="text" class="form-control" id="alamat" name="alamat" 
-                                    value="<?php echo $alamat;?>">
+                                    value="<?php $kolom['alamat']?>">
 								</div>
 								<br />
 								<!-- NEXT-BTN -->
@@ -112,7 +97,7 @@
 								<div class="form-group">
 									<label for="jurusan">Jurusan</label>
 									<input type="text" class="form-control" name="jurusan" id="jurusan"
-                                    value="<?php echo $jurusan;?>">
+                                    value="<?php $kolom['jurusan']?>">
 								</div>
 								<!-- Lama study---------- -->
 								<div class="form-group">
@@ -120,11 +105,11 @@
 									<div class="row">
 										<div class="col">
 											<input type="text" class="form-control" name="lama-studi-thn"
-												id="lama-studi" placeholder="Tahun" value="<?php echo $lamaStudiThn;?>">
+												id="lama-studi" placeholder="Tahun" value="<?php $kolom['lama_studi_thn']?>">
 										</div>
 										<div class="col">
 											<input type="text" class="form-control" name="lama-studi-bln"
-												id="lama-studi" placeholder="Bulan" value="<?php echo $lamaStudiBln;?>">
+												id="lama-studi" placeholder="Bulan" value="<?php $kolom['lama_studi_bln']?>">
 										</div>
 									</div>
 								</div>
@@ -132,14 +117,14 @@
 								<div class="form-group">
 									<label for="ipk">IPK Terakhir</label>
 									<input type="text" class="form-control" name="ipk" id="ipk" 
-									placeholder="4.00 (Gunakan titik sebagai koma)" value="<?php echo $ipk;?>">
+									placeholder="4.00 (Gunakan titik sebagai koma)" value="<?php $kolom['ipk']?>">
 								</div>
 								<!-- DESKRIPSI -->
 								<div class="form-group">
 									<label for="deskripsi">Tentang Anda</label>
 									<textarea class="form-control" id="deskripsi" name="deskripsi" rows="5"
 									placeholder="Tuliskan tentang diri Anda kurang dari 250 karakter"
-                                    value="<?php echo $deskripsi;?>"></textarea>
+                                    value="<?php $kolom['desc_diri']?>"></textarea>
 								</div>
 								<br />
 								<!-- PREV BTN -->
@@ -158,6 +143,13 @@
 		</div>
 	</div>
 	<!-- END OF FORM -->
+    <?php
+    }
+}
+        else{
+            echo "0 result";
+        }
+    ?>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
