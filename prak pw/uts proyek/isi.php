ffebc0278c672f3093da7004f9da9e2078a678d8
puay12 <puayyssii12@gmail.com>
2021-04-19 10:27:03 +0800

    modified:   uts proyek/isi.php

--- prak pw/uts proyek/isi.php
+++ prak pw/uts proyek/isi.php
@@ -16,30 +16,12 @@
     $sql = "INSERT INTO mahasiswa(nrp, nama, tmpt_lahir, tgl_lahir, gender, agama, alamat, jurusan, lama_studi_thn, 
     lama_studi_bln, ipk, desc_diri) VALUES ('$nrp', '$nama', '$tmptLahir', '$tglLahir', '$gender', 
     '$agama', '$alamat', '$jurusan', '$lamaStudiThn', '$lamaStudiBln', '$ipk', '$deskripsi')"; 
+
+    if(mysqli_query($konek, $sql)){
+        header("location:tampil.php");
+    }
+    else{
+        echo "<h1>Error : " . $sql . "<br/>" . mysqli_error($konek) . "</h1>";
+    }
+    mysqli_close($konek);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Status</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <link rel="stylesheet" href="form.css">
-</head>
-
-<body>
-    <div class="isi-sec">
-        <?php
-            if(mysqli_query($konek, $sql)){
-                header("location:tampil.php");
-            }
-            else{
-                echo "<h1>Error : " . $sql . "<br/>" . mysqli_error($konek) . "</h1>";
-            }
-            mysqli_close($konek);
-        ?>
-    </div>
-</body>
-</html>
