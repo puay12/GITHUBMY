110ed74fc4ae78e6600109f332a9974dc8378e98
puay12 <puayyssii12@gmail.com>
2021-05-03 10:34:30 +0800

    modified:   asset/css/main.css 	modified:   login.php

--- prak pw/prak mg 9/login.php
+++ prak pw/prak mg 9/login.php
@@ -58,7 +58,7 @@ if(isset($_POST['userid']) && isset($_POST['userpswd'])){
 					<input type="password" class="form-control" name="userpswd" id="pswd">
 				</div>
                 <button class="btn btn-main" type="submit">Masuk</button>
-                <h5 class="text-center">Belum punya akun? <a href="regist.php">Daftar</a></h5>
+                <h6 class="text-center">Belum punya akun? <a href="regist.php">Daftar</a></h6>
             </form>
         </div>
     </div>
