87923548d6523dde3458fbd209b57fb198254528
puay12 <puayyssii12@gmail.com>
2021-05-17 10:53:17 +0800

    prak9

--- prak pw/prak mg 9/asset/js/regist.js
+++
@@ -1,9 +0,0 @@
-$('#nextbtn').on('click', function() {
-    $('.step').addClass('hide-form').removeClass('show-form');
-    $('.step2').addClass('show-form').removeClass('hide-form');
-});
-
-$('#prevbtn').on('click', function() {
-    $('.step2').addClass('hide-form').removeClass('show-form');
-    $('.step').addClass('show-form').removeClass('hide-form');
-});
