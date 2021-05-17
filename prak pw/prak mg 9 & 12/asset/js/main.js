f3f5d35d243d7f673c52cfb1e5fef581909d8ccd
puay12 <puayyssii12@gmail.com>
2021-05-17 10:56:47 +0800

    modified:   asset/js/main.js

--- prak pw/prak mg 9 & 12/asset/js/main.js
+++ prak pw/prak mg 9 & 12/asset/js/main.js
@@ -3,6 +3,7 @@ $('.link1').on('click', function() {
     $('.link3').removeClass('active');
     $('.link1').addClass('active');
     $('.content-datmhs').removeClass('show').addClass('hides');
+    $('.content-datugas').removeClass('show').addClass('hides');
     $('.content-profil').removeClass('hides').addClass('show');
 });
 
@@ -11,6 +12,7 @@ $('.link2').on('click', function() {
     $('.link3').removeClass('active');
     $('.link2').addClass('active');
     $('.content-profil').removeClass('show').addClass('hides');
+    $('.content-datugas').removeClass('show').addClass('hides');
     $('.content-datmhs').removeClass('hides').addClass('show');
 });
 
@@ -18,9 +20,7 @@ $('.link3').on('click', function() {
     $('.link1').removeClass('active');
     $('.link2').removeClass('active');
     $('.link3').addClass('active');
+    $('.content-profil').removeClass('show').addClass('hides');
+    $('.content-datmhs').removeClass('show').addClass('hides');
+    $('.content-datugas').removeClass('hides').addClass('show');
 });
-
-// $('.prev').on('click', function(){
-//     $('.content-datmhs').removeClass('hides').addClass('show');
-//     $('.content-detail').removeClass('show').addClass('hides');
-// });
