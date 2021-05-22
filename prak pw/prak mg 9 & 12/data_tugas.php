c7718816301225c8997a6decddf0f92eb0cbd3af
puay12 <puayyssii12@gmail.com>
2021-05-22 21:53:02 +0800

    modified:   data_tugas.php

--- prak pw/prak mg 9 & 12/data_tugas.php
+++ prak pw/prak mg 9 & 12/data_tugas.php
@@ -9,7 +9,21 @@
             exit;
         }
     }
-    $idFile = $_GET['idFile'];
+    if(isset($_GET['idFile'])){
+        include 'config/koneksi.php';
+        $idFile = $_GET['idFile'];
+        $sql = "SELECT namaFile, sizeFile, typeFile, descript, content
+                FROM data_tugas
+                WHERE idFile = '$idFile'";
+        $result = mysqli_query($konek, $sql) or die('Error. query failed');
+        list($namaFile, $typeFile, $sizeFile, $content) = mysqli_fetch_array($result);
+        header("content-disposition:attachment;filename:$namaFile");
+        header("content-length:$sizeFile");
+        header("content-type:$typeFile");
+        echo $content;
+        include 'config/closedb.php';
+        exit;
+    }
 ?>
 
 <!DOCTYPE html>
